f836fbe4bdb66b2ca06cd54ed7adc0941fd5006b
Anshika Soni <anshikasoni357@gmail.com>
2024-12-03 21:56:06 +0530

    setting controllers

--- src/main/java/com/quizo/quizapp/QuestionController.java
+++ src/main/java/com/quizo/quizapp/QuestionController.java
@@ -1,8 +1,16 @@
 package com.quizo.quizapp;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController // as to accept the request,  whatever the question request will come this will handle
+@RequestMapping("question") // question is the path.
+
 public class QuestionController {
+    @GetMapping("allQuestions")
+    public String getAllQuestions(){
+        return "Hi, These are your questions!";
+    }
     
 }
